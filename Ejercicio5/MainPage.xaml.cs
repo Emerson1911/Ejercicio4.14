18a5a12c222a238599f3618ebc4b024f186c01f5
Emers <Emers@DESKTOP-RC7M2CL>
2023-06-19 15:54:42 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio5/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace Ejercicio5;
+
+public partial class MainPage : ContentPage
+{
+	
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Calcular_Clicked(object sender, EventArgs e)
+    {
+		if (!string.IsNullOrEmpty(numer1.Text))
+		{
+			var numero = Convert.ToInt32(numer1.Text);
+			var nur1 = Math.Pow(numero, 3);
+			var nur2 = Math.Pow(numero, 2);
+			cubo.Text = Convert.ToString(nur1);
+			cuadrado.Text = Convert.ToString(nur2);
+		}
+		else
+		{
+			DisplayAlert("Eror", "Escribe tu numero primero ciego","Adoptado");
+		}
+    }
+}
+
